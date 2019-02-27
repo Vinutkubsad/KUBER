646d25fa8f631a9e239dc29f4e8cc69dacdfc97e
smithasmi95 <smitha.r@accionlabs.com>
2019-02-27 20:26:41 +0530

    ...

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -46,6 +46,6 @@ export class FooterComponent implements OnInit {
     this.service.sendMessage(data).subscribe((Response:any)=>{
       console.log(Response);
     });
-    // this.refresh();
+   
   }
 }
