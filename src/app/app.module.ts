c84dfe2006084fc94b125352e5cc4fc8a9a741f1
smithasmi95 <smitha.r@accionlabs.com>
2019-03-12 18:28:31 +0530

    merged

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { NgxPaginationModule}  from 'ngx-pagination';
+// import { NgxPaginationModule}  from 'ngx-pagination';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-import { ModalModule } from 'ngx-bootstrap/modal';
+// import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+// import { ModalModule } from 'ngx-bootstrap/modal';
 import { DataService } from './services/data.service';
 import {NgbModule,NgbPaginationModule} from '@ng-bootstrap/ng-bootstrap';
 // import { NgbdDatepickerPopup } from './datepicker-popup';
@@ -70,10 +70,10 @@ import { ProfileComponent } from './dashboard/profile/profile.component';
     ReactiveFormsModule,
     RouterModule.forRoot(routes, {useHash:true}),
     HttpClientModule,
-    NgxPaginationModule,
+    // NgxPaginationModule,
     FormsModule,
-    BsDatepickerModule.forRoot(),
-    ModalModule.forRoot(),
+    // BsDatepickerModule.forRoot(),
+    // ModalModule.forRoot(),
     NgbModule,
     NgbPaginationModule
     
